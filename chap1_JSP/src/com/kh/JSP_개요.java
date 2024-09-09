438dc331bac571f66d3b7dc0da17885a80f4f5e9
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-09-09 09:48:31 +0900

    JSP 개요

---
+++ chap1_JSP/src/com/kh/JSP_개요.java
@@ -0,0 +1,14 @@
+package com.kh;
+/*
+JSP : Java Server Pages
+HTML 코드 안에서 자바 코드를 작성할 수 있음
+모든 Java 코드까지 HTML 작성하는 페이지 안에서 작성하는 것이 아니라 부분적으로 필요한 기능만 Java 로 작성
+JSP 와 같은 프로젝트를 진행하기 위해서는 
+ThymeLeaf 와 같은 환경으로 사용
+Dynamic Web Project OR Springboot 환경으로 실행
+
+ThymeLeaf -> JSP 작성
+*/
+public class JSP_개요 {
+
+}
